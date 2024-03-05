7fa475bb2910e4ab0dbadfd2bc359c2912f11674
exploreli <735783177@qq.com>
2024-03-05 20:33:26 +0800

    添加实现agent

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # pptagent
+生成ppt的agent，做了一些简单的实现，有待进一步完善
+## 配置鉴权参数
+修改 setkey.sh
+## 启动项目
+python pptagent.py
+
+执行后会在项目根目录生成一个pptx
+
+
---
+++ setkey.sh
@@ -0,0 +1,5 @@
+# 配置自己的key，推荐gpt、文心千帆、阿里百练，修改自己对应的鉴权配置
+export AGENT_KEY=''
+export APP_ID=''
+export ACCESS_KEY_ID=''
+export ACCESS_KEY_SECRET=''
